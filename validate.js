f0abbeeb8a2a22a4e335cb5c19c39ff5182b9c8d
Flor Ballinas <flor_ballinas@hotmail.com>
2019-05-16 15:34:45 -0500

    provando validacion

--- validate.js
+++ validate.js
@@ -1,19 +1,34 @@
-const surveyForm = () => {
-  const form = document.getElementsByName('survey-form')[0];
-  const elementsForm = form.elements;
-  const buttom = document.getElementById('btn-submit');
-  
-const validateName = (e) => {
-  if(form.name.value === 0) {
-    alert('completa el campo nombre');
-    e.preventDefault();
-  }
-};
+// const surveyForm = () => {
 
-  const validate = (e) => {
-    validateName(e);
-  }
+//   const validateName = (e) => {
+//     if (form.name.value == 0) {
+//       alert('hola')
+//       e.preventDefault();
+//     }
+//   };
+//   const validate = (e) => {
+//     validateName(e);
+//   };
 
-  form.addEventListener('submit', validate);
-};
-surveyForm();
+//   form.addEventListener('submit', validate);
+// };
+// alert('flor');
+
+const form = document.getElementById('survey-form');
+const elementsForm = form.elements;
+const buttom = document.getElementById('btn-submit');
+
+window.addEventListener('load', start, false);
+
+function start() {
+  form.addEventListener('submit', validate, false);
+}
+
+function validate(evt) {
+  var cla1 = form.name.value;
+  var cla2 = form.email.value;
+  if (cla1 !== cla2) {
+    alert('Las claves ingresadas son distintas');
+    evt.preventDefault();
+  }
+}
